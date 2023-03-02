9d82b2fb83f3202bf6e81251f94120d0f898c639
Vitali B <bukhtiyarov_01@mail.ru>
2023-03-02 22:10:22 +0300

    add url /blog/ in app 'blog'

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def posts_list(request):
+    names = ['oleg','oly']
+    return render(request, 'blog/index.html', context={'names':names})
