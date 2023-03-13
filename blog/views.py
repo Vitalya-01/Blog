09c92ae9a29787be8150d140f516a9890cb0174f
Vitali B <bukhtiyarov_01@mail.ru>
2023-03-13 22:05:57 +0300

    add function Delete Post, Tag

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
+from django.urls import reverse
 
 from .models import Post, Tag
 from .utils import *
@@ -21,6 +22,11 @@ class PostUpdate(ObjectUpdateMixin, View):
     model_form = PostForm
     template = 'blog/post_update_form.html'
 
+class PostDelete(ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete_form.html'
+    redirect_url = 'posts_list_url'
+
 
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
@@ -36,6 +42,12 @@ class TagUpdate(ObjectUpdateMixin, View):
     model = Tag
     model_form = TagForm
     template = 'blog/tag_update_form.html'
+
+
+class TagDelete(ObjectDeleteMixin,View):
+    model = Tag
+    template = 'blog/tag_delete_form.html'
+    redirect_url = 'tags_list_url'
 
 
 def posts_list(request):
